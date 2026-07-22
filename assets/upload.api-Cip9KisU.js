422bf00d20196c8bdd19f18424aad2d6edd2cfdf
Wuan21 <qn21012004@gmail.com>
2026-07-22 20:39:09 +0000

    Deploying to gh-pages from @ Wuan21/SyncQuiz@8154b8407a0ab392af30c69c38db1564eaf1b45d 🚀

---
+++ assets/upload.api-Cip9KisU.js
@@ -0,0 +1,1 @@
+import{o as e}from"./index-CNbYOPpD.js";var t=t=>e.post(`/upload/presign`,t).then(e=>e.data),n=()=>e.get(`/analytics/dashboard`).then(e=>e.data),r=()=>e.get(`/categories`).then(e=>e.data);export{r as n,t as r,n as t};
